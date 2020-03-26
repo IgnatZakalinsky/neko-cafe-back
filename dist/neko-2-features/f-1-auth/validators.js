610a0a1a4feb269f22dc34b33a4b93292c8cc706
ai73aaa <1qazxcvBG>
2020-03-26 18:44:28 +0300

    refactoring

--- dist/neko-2-features/f-1-auth/validators.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const reg = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,7}$/i;
-exports.emailValidator = (email) => reg.test(email); // true - valid
-exports.passwordValidator = (password) => password.length > 7; // true - valid
-//# sourceMappingURL=validators.js.map
