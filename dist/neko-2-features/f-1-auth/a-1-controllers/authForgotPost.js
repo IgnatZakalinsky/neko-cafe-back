b3abb93adc04d3ca6bba46a19ea9944e8ae0403c
ai73aaa <1qazxcvBG>
2020-03-26 15:16:22 +0300

    test gmail

--- dist/neko-2-features/f-1-auth/a-1-controllers/authForgotPost.js
+++ dist/neko-2-features/f-1-auth/a-1-controllers/authForgotPost.js
@@ -14,13 +14,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const user_1 = __importDefault(require("../a-2-models/user"));
 const app_1 = require("../../../neko-1-config/app");
+const nodemailer_1 = __importDefault(require("nodemailer"));
 exports.generateNewPassword = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const user = yield user_1.default.findOne({ email: req.body.email }).exec();
         if (!user)
             res.status(404).json({ error: 'Email address not found', in: 'generateNewPassword' });
         else {
-            res.status(500).json({ error: "sorry, I can't send new password on your email" });
+            // res.status(500).json({error: "sorry, I can't send new password on your email"});
+            const transporter = nodemailer_1.default.createTransport({
+                service: 'gmail',
+                // host: 'smtp.ethereal.email',
+                // port: 587,
+                // secure: false,
+                auth: {
+                    user: process.env.GMAIL_USER || '',
+                    pass: process.env.GMAIL_PASS || ''
+                }
+            });
+            const info = transporter.sendMail({
+                from: 'Neko-cafe',
+                to: 'ai73a@yandex.ru',
+                subject: 'gmail test',
+                text: 'test text',
+            });
+            console.log('gmail info: ', info);
         }
     }
     catch (e) {
--- src/neko-2-features/f-1-auth/a-1-controllers/authForgotPost.ts
+++ src/neko-2-features/f-1-auth/a-1-controllers/authForgotPost.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
 import User, {IUser} from "../a-2-models/user";
 import {DEV_VERSION} from "../../../neko-1-config/app";
+import nodeMailer from "nodemailer";
 
 export const generateNewPassword = async (req: Request, res: Response) => {
     try {
@@ -9,7 +10,28 @@ export const generateNewPassword = async (req: Request, res: Response) => {
         if (!user) res.status(404).json({error: 'Email address not found', in: 'generateNewPassword'});
 
         else {
-            res.status(500).json({error: "sorry, I can't send new password on your email"});
+            // res.status(500).json({error: "sorry, I can't send new password on your email"});
+
+            const transporter = nodeMailer.createTransport({
+                service: 'gmail',
+                // host: 'smtp.ethereal.email',
+                // port: 587,
+                // secure: false,
+                auth: {
+                    user: process.env.GMAIL_USER || '',
+                    pass: process.env.GMAIL_PASS || ''
+                }
+            });
+
+            const info = transporter.sendMail({
+                from: 'Neko-cafe',
+                to: 'ai73a@yandex.ru',
+                subject: 'gmail test',
+                text: 'test text',
+                // html: '<div style="color: lime; background-color: black">test html</div>',
+            });
+
+            console.log('gmail info: ', info)
 
         }
     } catch (e) {
