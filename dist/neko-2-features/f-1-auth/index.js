2c6c053a39f670c6d617dccadb97cdd3ee820f84
ai73aaa <1qazxcvBG>
2020-04-15 20:07:16 +0300

    test ru ок

--- dist/neko-2-features/f-1-auth/index.js
+++ dist/neko-2-features/f-1-auth/index.js
@@ -18,15 +18,30 @@ auth.post('/register', createUser_1.createUser);
 auth.post('/me', findUserByToken_1.findUserByToken(getMe_1.getMe, 'getMe'));
 auth.post('/forgot', passwordRecovery_1.passwordRecovery);
 auth.post('/set-new-password', setNewPassword_1.setNewPassword);
-auth.get('/test', (req, res) => {
+auth.post('/test', (req, res) => {
     if (req.body.success) {
+        res.status(200).json({
+            errorText: '...всё ок)',
+            info: 'код 200 - обычно означает что скорее всего всё ок)',
+            yourBody: req.body,
+            yourQuery: req.query
+        });
     }
     else if (req.body.success === undefined) {
-        res.status(500).json({
-            errorText: 'Ты не отправил success в body вообще!'
+        res.status(400).json({
+            errorText: 'Ты не отправил success в body вообще!',
+            info: 'ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!',
+            yourBody: req.body,
+            yourQuery: req.query
         });
     }
     else {
+        res.status(500).json({
+            errorText: 'эмитация ошибки на сервере',
+            info: 'ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)',
+            yourBody: req.body,
+            yourQuery: req.query
+        });
     }
 });
 exports.default = auth;
--- src/neko-2-features/f-1-auth/index.ts
+++ src/neko-2-features/f-1-auth/index.ts
@@ -17,15 +17,28 @@ auth.post('/me', findUserByToken(getMe, 'getMe'));
 auth.post('/forgot', passwordRecovery);
 auth.post('/set-new-password', setNewPassword);
 
-auth.get('/test', (req: Request, res: Response) => {
+auth.post('/test', (req: Request, res: Response) => {
     if (req.body.success) {
-
+        res.status(200).json({
+            errorText: '...всё ок)',
+            info: 'код 200 - обычно означает что скорее всего всё ок)',
+            yourBody: req.body,
+            yourQuery: req.query
+        })
     } else if (req.body.success === undefined) {
-        res.status(500).json({
-            errorText: 'Ты не отправил success в body вообще!'
+        res.status(400).json({
+            errorText: 'Ты не отправил success в body вообще!',
+            info: 'ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!',
+            yourBody: req.body,
+            yourQuery: req.query
         })
     } else {
-
+        res.status(500).json({
+            errorText: 'эмитация ошибки на сервере',
+            info: 'ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)',
+            yourBody: req.body,
+            yourQuery: req.query
+        })
     }
 });
 
